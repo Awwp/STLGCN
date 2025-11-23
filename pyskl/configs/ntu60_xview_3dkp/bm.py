9b48299935f5b2bb1108fbc939d2f7c361fdbda4
Awwp <94427296+Awwp@users.noreply.github.com>
2025-11-23 11:19:42 +0800

    Update bm.py

--- pyskl/configs/ntu60_xview_3dkp/bm.py
+++ pyskl/configs/ntu60_xview_3dkp/bm.py
@@ -1,11 +1,11 @@
 modality = 'bm'
 graph = 'nturgb+d'
-work_dir = f'./work_dirs/lpmdgcn/ntu60_xview_3dkp/bm'
+work_dir = f'./work_dirs/stlgcn/ntu60_xview_3dkp/bm'
 
 model = dict(
     type='RecognizerGCN',
     backbone=dict(
-        type='LPMDGCN',
+        type='STLGCN',
         gcn_ratio=0.125,
         gcn_ctr='T',
         gcn_ada='T',
