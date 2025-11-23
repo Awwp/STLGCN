5237205af2b2c64054223fc5d3effc4fd558daad
Awwp <94427296+Awwp@users.noreply.github.com>
2025-11-23 11:20:15 +0800

    Update jm.py

--- pyskl/configs/ntu60_xview_3dkp/jm.py
+++ pyskl/configs/ntu60_xview_3dkp/jm.py
@@ -1,11 +1,11 @@
 modality = 'jm'
 graph = 'nturgb+d'
-work_dir = f'./work_dirs/lpmdgcn/ntu60_xview_3dkp/jm'
+work_dir = f'./work_dirs/stlgcn/ntu60_xview_3dkp/jm'
 
 model = dict(
     type='RecognizerGCN',
     backbone=dict(
-        type='LPMDGCN',
+        type='STLGCN',
         gcn_ratio=0.125,
         gcn_ctr='T',
         gcn_ada='T',
