df139fadee8df7c5da1cf589ee8d231c91834351
Awwp <94427296+Awwp@users.noreply.github.com>
2025-11-23 11:17:58 +0800

    Update jm.py

--- pyskl/configs/ntu120_xsub_3dkp/jm.py
+++ pyskl/configs/ntu120_xsub_3dkp/jm.py
@@ -1,11 +1,11 @@
 modality = 'jm'
 graph = 'nturgb+d'
-work_dir = f'./work_dirs/lpmdgcn/ntu120_xsub_3dkp/jm'
+work_dir = f'./work_dirs/stlgcn/ntu120_xsub_3dkp/jm'
 
 model = dict(
     type='RecognizerGCN',
     backbone=dict(
-        type='LPMDGCN',
+        type='STLGCN',
         gcn_ratio=0.125,
         gcn_ctr='T',
         gcn_ada='T',
